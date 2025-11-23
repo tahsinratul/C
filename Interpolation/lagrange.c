668fbd2ec2a947fd2634ffe41d0bfee8b1fb321c
tahsinratul <ratulprofessional123@gmail.com>
2025-11-23 21:28:48 +0600

    Interpolation

---
+++ Interpolation/lagrange.c
@@ -0,0 +1,29 @@
+// Lagrange iteration
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int i, j, n;
+    float sum = 0.0, a, x[10], y[10], prod;
+    printf("enter the number of data \n");
+    scanf("%d", &n);
+    printf("enter the values of data (independent)\n");
+    for (i = 0; i < n; ++i)
+        scanf("%f", &x[i]);
+    printf("enter the value of data (dependent)\n");
+    for (i = 0; i < n; ++i)
+        scanf("%f", &y[i]);
+    printf("enter the value where we want to interpolate\n");
+    scanf("%f11.3", &a);
+    for (i = 0; i <= n; ++i)
+    {
+        prod = 1.0;
+        for (j = 0; j <= n; ++j)
+        {
+            if (i != j)
+                prod = prod * ((a - x[j]) / (x[i] - x[j]));
+        }
+        sum = sum + prod * y[i];
+    }
+    printf("the interpolated value at %f is %f \n", a, sum);
+}
---
+++ Interpolation/newtonB.c
@@ -0,0 +1,58 @@
+// Newton backword
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    float f(float, int);
+    int fact(int);
+    float x[10], y[10], d[10][10], u, a, sum = 0.0, h, tsum;
+    int i, j, n;
+    printf("enter the number of data\n");
+    scanf("%d", &n);
+    printf("enter the initial value of x\n");
+    scanf("%f", &x[0]);
+    printf("enter the differences\n");
+    scanf("%f", &h);
+    for (i = 0; i < n; i++)
+        x[i + 1] = x[i] + h;
+    printf("enter the value of y\n");
+    for (i = 0; i < n; i++)
+        scanf("%f", &y[i]);
+    for (i = 0; i < n; i++)
+        d[0][i] = y[i];
+    for (i = 1; i < n; i++)
+    {
+        for (j = 0; j < n - i; j++)
+        {
+            d[i][j] = d[i - 1][j + 1] - d[i - 1][j];
+            printf("d[%d][%d]=%f\t", i, j, d[i][j]);
+        }
+        printf("\n\n");
+    }
+    printf("enter the value where we interpolate y\n");
+    scanf("%f", &a);
+    u = (a - x[n - 1]) / h;
+    for (i = 1; i < n; i++)
+    {
+        sum = sum + (f(u, i) * d[i][n - 1 - i]) / fact(i);
+        printf("%f\n", sum);
+    }
+    tsum = y[n - 1] + sum;
+    printf("the required value is y[%.4f]=%.4f\n", a, tsum);
+    return (0);
+}
+float f(float x, int i)
+{
+    int l;
+    float prod = x;
+    for (l = 1; l < i; l++)
+        prod = prod * (x + l);
+    return (prod);
+}
+int fact(int k)
+{
+    int prod = 1, i;
+    for (i = 1; i <= k; i++)
+        prod = prod * i;
+    return (prod);
+}
